f9576657d2411dff952d6b15773c1c461898b462
Demonthos <66571940+Demonthos@users.noreply.github.com>
2021-04-12 12:44:28 -0500

    added __name__ == '__main__'

--- main.py
+++ main.py
@@ -75,79 +75,80 @@ def linearApprox(maxSize=10, intVals=True, returnCached=False):
     return linearApproxDecorator
 
 
-lossSum = 0
-count = 0
-results = []
-delay = 0.00001
-# power = random.randint(3, 3000)
-power = 37
-num = 1000
+if __name__ == '__main__':
+    lossSum = 0
+    count = 0
+    results = []
+    delay = 0.00001
+    # power = random.randint(3, 3000)
+    power = 37
+    num = 1000
 
 
-def sq(x):
-    time.sleep(delay)
-    return x ** power
-    # return math.factorial(x)
+    def sq(x):
+        time.sleep(delay)
+        return x ** power
+        # return math.factorial(x)
 
 
-@linearApprox(10, returnCached=True)
-def sqla(x):
-    time.sleep(delay)
-    return x ** power
-    # return math.factorial(x)
+    @linearApprox(10, returnCached=True)
+    def sqla(x):
+        time.sleep(delay)
+        return x ** power
+        # return math.factorial(x)
 
 
-l = list(range(num))
-random.shuffle(l)
-l1 = list(range(0, num, 9))
-l2 = [e for e in l if e not in l1]
-random.shuffle(l2)
+    l = list(range(num))
+    random.shuffle(l)
+    l1 = list(range(0, num, 9))
+    l2 = [e for e in l if e not in l1]
+    random.shuffle(l2)
 
-t1 = time.time()
+    t1 = time.time()
 
-for i in l:
-    result = sq(i)
-    print(result)
-    results.append(result)
+    for i in l:
+        result = sq(i)
+        print(result)
+        results.append(result)
 
-t1FinishTime = time.time()
-t2 = time.time()
-pc1 = 0
-# for i in l1:
-#     print(sqla(i))
-# for i in l2:
-#     print(sqla(i))
-for e in l:
-    x1, x2 = sqla(e)
-    print(x1)
-    if x2:
-        pc1 += 1
+    t1FinishTime = time.time()
+    t2 = time.time()
+    pc1 = 0
+    # for i in l1:
+    #     print(sqla(i))
+    # for i in l2:
+    #     print(sqla(i))
+    for e in l:
+        x1, x2 = sqla(e)
+        print(x1)
+        if x2:
+            pc1 += 1
 
-t2FinishTime = time.time()
+    t2FinishTime = time.time()
 
-t3 = time.time()
-pc2 = 0
-for i, e in enumerate(l):
-    x1, x2 = sqla(e)
-    try:
-        loss = (abs(results[i] - x1) / abs(results[i]))
-        lossSum += loss
-        count += 1
-        print(loss, ' ', x1)
-    except ZeroDivisionError:
-        pass
-    if x2:
-        pc2 += 1
+    t3 = time.time()
+    pc2 = 0
+    for i, e in enumerate(l):
+        x1, x2 = sqla(e)
+        try:
+            loss = (abs(results[i] - x1) / abs(results[i]))
+            lossSum += loss
+            count += 1
+            print(loss, ' ', x1)
+        except ZeroDivisionError:
+            pass
+        if x2:
+            pc2 += 1
 
-t3FinishTime = time.time()
+    t3FinishTime = time.time()
 
-print()
-print()
-# print(time.time())
-print('full calculations:', t1FinishTime - t1)
-print('full approximation:', t2FinishTime - t2, pc1)
-print('cached approximation:', t3FinishTime - t3, pc2)
-print(lossSum/count)
+    print()
+    print()
+    # print(time.time())
+    print('full calculations:', t1FinishTime - t1)
+    print('full approximation:', t2FinishTime - t2, pc1)
+    print('cached approximation:', t3FinishTime - t3, pc2)
+    print(lossSum/count)
 
-# while True:
-#     print(sqla(int(input())))
+    # while True:
+    #     print(sqla(int(input())))
